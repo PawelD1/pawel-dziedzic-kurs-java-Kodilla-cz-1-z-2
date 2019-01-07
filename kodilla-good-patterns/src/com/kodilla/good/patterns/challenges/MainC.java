ab4982793b9bbd1b5da7f09a13c07acc45759302
LENOVO-PC\Paweł <pawel.dz.m@interia.pl>
2019-01-07 10:32:53 +0100

    Stream 7.01.19

--- kodilla-good-patterns/src/com/kodilla/good/patterns/challenges/MainC.java
+++ kodilla-good-patterns/src/com/kodilla/good/patterns/challenges/MainC.java
@@ -7,17 +7,30 @@ public class MainC
     public static void main(String [] args)
     {
         MovieStore ms=new MovieStore();
-       // Map<String, List<String>> titles=new HashMap<>();
-       // titles=ms.getMovies();
-       // Map<String, List<String>> allTitles = new HashMap<String, List<String>>();
-        //ms.getMovies().stream()
-        Map<String, List<String>> theResultMapOfMovie= ms.getMap().stream()
+       Map<String, List<String>> titles=new HashMap<>();
+        titles=ms.getMovies();
+        Map<String, List<String>> theResultMapOfMovie= titles;
+        System.out.println("PIERWSZY SPOSÓB");
+        titles.entrySet().stream()
                         .map(s -> s+"!")
-                         .forEach(System.out::println);
+                         .forEach(System.out::print);
+        System.out.println("\nDRUGI SPOSÓB");
                 theResultMapOfMovie.entrySet().stream()
                         .map(entry -> entry.getKey() + ": " + entry.getValue())
                         .map(s->s+"!")
-                        .forEach(System.out::println);
+                        .forEach(System.out::print);
+        System.out.println("\nTRZECI SPOSÓB");
+        theResultMapOfMovie.keySet().stream()
+            .map(s->s+"!")
+            .forEach(System.out::print);
+        theResultMapOfMovie.values().stream()
+                .map(s->s+"!")
+                .forEach(System.out::print);
+        System.out.println("\nCZWARTY SPOSÓB");
+        theResultMapOfMovie.values().stream()
+                .map(s->s+"!")
+                .forEach(System.out::print);
+
             }
         }
 
