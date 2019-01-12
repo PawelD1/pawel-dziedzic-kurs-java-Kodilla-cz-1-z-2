4a2a754d6a0dd1e16aeb60530153390dfef9d8fd
LENOVO-PC\Paweł <pawel.dz.m@interia.pl>
2019-01-12 13:05:36 +0100

    Third commit 12.01.19

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/TaskList.java
@@ -0,0 +1,13 @@
+package com.kodilla.spring.portfolio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class TaskList {
+    List<String> tasks;
+
+    public TaskList(List<String> tasks) {
+        this.tasks = new ArrayList<>();
+        ;
+    }
+}
---
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -0,0 +1,38 @@
+package com.kodilla.spring.portfolio;
+
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BoardTestSuite
+{
+    @Test
+    public void testTaskAdd()
+    {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
+        Board board = context.getBean(Board.class);
+
+        List<String> listOfTasksToDo=new ArrayList<>();
+        listOfTasksToDo.add("Project");
+        board.toDoList.tasks= listOfTasksToDo;
+
+        List<String> listOfTasksInProgress=new ArrayList<>();
+        listOfTasksInProgress.add("Game");
+        board.inProgressList.tasks=listOfTasksInProgress;
+
+        List<String> listOfTasksDone=new ArrayList<>();
+        listOfTasksDone.add("Homework");
+        board.doneList.tasks=listOfTasksInProgress;
+
+
+
+
+
+    }
+}
